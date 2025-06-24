ea6f5fe94f832cf439ca8cb7a816542d5fcb2bce
Jinwoo Han <qew8502@gmail.com>
2025-06-24 01:41:32 +0000

    feat: implement rbtree min/max

--- src/rbtree.c
+++ src/rbtree.c
@@ -302,14 +302,28 @@ node_t *rbtree_find(const rbtree *t, const key_t key)
 
 node_t *rbtree_min(const rbtree *t)
 {
-  // TODO: implement find
-  return t->root;
+  node_t *current = t->root;
+
+  while (current != t->nil)
+  {
+    current = current->left;
+  }
+
+  return current;
 }
 
 node_t *rbtree_max(const rbtree *t)
 {
-  // TODO: implement find
-  return t->root;
+  node_t *current = t->root;
+
+  while (current != t->nil)
+  {
+    current = current->right;
+  }
+
+  return current;
+}
+
 }
 
 int rbtree_erase(rbtree *t, node_t *p)
